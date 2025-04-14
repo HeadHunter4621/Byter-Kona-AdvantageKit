bc4c9b772e0d9aca69c3df290aa5c3126d2463f9
Kona <81715373+HeadHunter4621@users.noreply.github.com>
2025-04-13 19:27:20 -0700

    YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE YIPPEE :3

--- src/main/java/frc/robot/Input.kt
+++ src/main/java/frc/robot/Input.kt
@@ -1,11 +1,36 @@
 package frc.robot
 
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController
+import frc.robot.subsystems.arm_roller.ArmRoller
+import frc.robot.subsystems.pneumatics.Arm
 
 object Input {
 
     private val controller = CommandXboxController(0)
 
+    /** Button Assignment **/
+    // Arm
+    private val rollerIntakeButton = controller.rightTrigger()    // Intake through Roller -> RT
+    private val rollerOuttakeButton = controller.rightBumper()    // Outtake through Roller -> RB
+
+    init {
+        /** Command Assignment **/
+        rollerIntakeButton.whileTrue(
+            Arm.armDownCommand().andThen(
+                ArmRoller.intakeCommand()))
+            .onFalse(
+                Arm.armUpCommand().andThen(
+                    ArmRoller.stopRollerCommand()))
+
+        rollerOuttakeButton.whileTrue(
+            Arm.armDownCommand().andThen(
+                ArmRoller.outtakeCommand()))
+            .onFalse(
+                Arm.armUpCommand().andThen(
+                    ArmRoller.stopRollerCommand()))
+    }
+
+    // Joysticks
     fun getLeftX(): Double {
         return controller.leftX
     }
